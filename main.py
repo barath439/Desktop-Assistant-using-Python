984031680be14c7f5772ab79b0ddeb6e83eebad3
Barath U <barathraju805@gmail.com>
2024-06-07 14:36:08 +0530

    message

--- main.py
+++ main.py
@@ -9,4 +9,17 @@ import os
 # Taking voice from my system
 engine=pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-print(voices)
+# print(voices[1].id)
+# print(type(voices))
+engine.setProperty('voice', voices[1].id)
+engine.setProperty('rate', 150)
+
+
+# Speak function
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+    
+# We will miss you from next week yathee maam
+speak("i am Barath, working as statistical programmer")
