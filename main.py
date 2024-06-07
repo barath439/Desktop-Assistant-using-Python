de969717f86793e663b9ea7f0a6055225e568b07
Barath U <barathraju805@gmail.com>
2024-06-07 10:28:54 +0530

    message

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pyttsx3
 import speech_recognition as sr
 import datetime
+import  webbrowser
 import wikipedia
+import os
+
+
+# Taking voice from my system
+engine=pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices)
